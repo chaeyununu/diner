c749a232b1e15561a1d8e60961ade0e80d4338cd
chaeyun <ipsac30333@gmail.com>
2026-05-28 01:44:59 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -395,7 +395,7 @@ function loadRabbid() {
       prepareMaterials(rabbid);
       rabbid.position.set(0.0501, -0.5050, 0.5041);
       rabbid.scale.setScalar(0.038);
-      rabbid.rotation.y = -4.676 + Math.PI - 0.27; // Rabbid looks a bit more to its own right
+      rabbid.rotation.y = -4.676 + Math.PI - 0.42; // Rabbid looks more to its own right
       scene.add(rabbid);
       addRabbidSoftLight(rabbid.position);
       if (gltf.animations && gltf.animations.length) {
