c6462273eb5ad3e2165f435ea6f9c961a1ed5d4a
chaeyun <ipsac30333@gmail.com>
2026-05-28 01:24:32 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ const FRONT_ENTRY = [
   { id: "P02", x: -3.5636, y: -0.7071, z: -2.0796, yaw:  0.104, pitch:  0.150, fov: 52 },
   { id: "P03", x: -3.5636, y: -0.7071, z: -2.0796, yaw: -0.672, pitch:  0.182, fov: 52 },
   { id: "P04", x: -3.5636, y: -0.7071, z: -2.0796, yaw: -1.412, pitch:  0.196, fov: 52 },
-  { id: "P05", x: -3.5636, y: -0.7071, z: -2.0796, yaw: -2.132, pitch:  0.235, fov: 52 },
+  { id: "P05", x: -3.5636, y: -0.7071, z: -2.0796, yaw: -2.132, pitch:  0.340, fov: 52 },
   { id: "P06", x: -3.5636, y: -0.7071, z: -2.0796, yaw: -1.120, pitch:  0.165, fov: 52 },
   { id: "P07", x: -2.3200, y: -0.7071, z: -1.9700, yaw: -1.080, pitch:  0.158, fov: 52 },
   { id: "P08", x: -1.0600, y: -0.7071, z: -1.8600, yaw: -1.060, pitch:  0.150, fov: 52 },
@@ -882,6 +882,7 @@ createUI();
 audioSystem = initAudio(setWeatherMode);
 loadDiner();
 animate();
+
 
 
 
