71e663835e03aa1dc7c8a005fb354c31cfe7e6fa
chaeyun <ipsac30333@gmail.com>
2026-05-28 01:38:15 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ const FRONT_ENTRY = [
   { id: "P02", x: -3.5636, y: -0.7071, z: -2.0796, yaw:  0.104, pitch:  0.150, fov: 52 },
   { id: "P03", x: -3.5636, y: -0.7071, z: -2.0796, yaw: -0.672, pitch:  0.182, fov: 52 },
   { id: "P04", x: -3.5636, y: -0.7071, z: -2.0796, yaw: -1.412, pitch:  0.196, fov: 52 },
-  { id: "P05", x: -3.5636, y: -0.7071, z: -2.0796, yaw: -2.132, pitch:  0.520, fov: 52 },
+  { id: "P05", x: -3.5636, y: -0.7071, z: -2.0796, yaw: -2.132, pitch:  0.178, fov: 52 },
   { id: "P06", x: -3.5636, y: -0.7071, z: -2.0796, yaw: -1.120, pitch:  0.165, fov: 52 },
   { id: "P07", x: -2.3200, y: -0.7071, z: -1.9700, yaw: -1.080, pitch:  0.158, fov: 52 },
   { id: "P08", x: -1.0600, y: -0.7071, z: -1.8600, yaw: -1.060, pitch:  0.150, fov: 52 },
@@ -395,7 +395,7 @@ function loadRabbid() {
       prepareMaterials(rabbid);
       rabbid.position.set(0.0501, -0.4850, 0.5041);
       rabbid.scale.setScalar(0.038);
-      rabbid.rotation.y = -4.676 + Math.PI;
+      rabbid.rotation.y = -4.676 + Math.PI - 0.18; // Rabbid looks slightly to its own right
       scene.add(rabbid);
       addRabbidSoftLight(rabbid.position);
       if (gltf.animations && gltf.animations.length) {
